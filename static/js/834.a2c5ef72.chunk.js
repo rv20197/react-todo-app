52d54659198a23bf65381bef623e9d8f04d7976c
Vatsal Dinesh Rajgor <rv20197@gmail.com>
2022-10-25 13:05:07 +0530

    Updates

---
+++ static/js/834.a2c5ef72.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktodo_app=self.webpackChunktodo_app||[]).push([[834],{834:function(n,t,e){e.r(t),e.d(t,{default:function(){return c}});var r=e(689),u=e(434),i=e(135),d=e(184);function c(){var n=(0,r.UO)().id,t=(0,u.v9)((function(t){return(0,i.U5)(t,n)}));return(0,d.jsx)("div",{style:{padding:"1rem"},children:t&&(0,d.jsx)("h2",{children:t.title})})}}}]);
+//# sourceMappingURL=834.a2c5ef72.chunk.js.map
